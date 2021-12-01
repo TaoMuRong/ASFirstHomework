e3f9663931bb573c8283f3c7b2d60f4ecf0463ab
TaoMuRong <13797500742@163.com>
2021-12-01 21:31:56 +0800

    使用百度LBS添加定位功能

--- app/src/main/java/com/example/homework/BookmarkFragment.java
+++ app/src/main/java/com/example/homework/BookmarkFragment.java
@@ -1,18 +1,90 @@
 package com.example.homework;
 
-import  android.os.Bundle;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.os.Build;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
+
 public class BookmarkFragment extends Fragment {
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap = null;
+    private LocationClient mLocationClient = null;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bookmark_fragment,container,false);
+        View view = inflater.inflate(R.layout.bookmark_fragment, container, false);
+
+        //获取地图控件引用
+        mMapView = (MapView) view.findViewById(R.id.bmapView);
+        //开启定位图层
+
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+
+        //定位初始化
+        mLocationClient = new LocationClient(view.getContext());
+
+
+
+
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+        //注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener(mBaiduMap,mMapView);
+        mLocationClient.registerLocationListener(myLocationListener);
+        //开启地图定位图层
+        mLocationClient.start();
+
+        return view;
+    }
+
+
+    @Override
+    public void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/homework/MyLocationListener.java
@@ -0,0 +1,44 @@
+package com.example.homework;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class MyLocationListener extends BDAbstractLocationListener {
+    private BaiduMap mBaiduMap;
+    private MapView mMapView;
+    private boolean isFirstLocate = true;
+
+    public MyLocationListener(BaiduMap mBaiduMap,MapView mMapView){
+        this.mBaiduMap=mBaiduMap;
+        this.mMapView=mMapView;
+
+    }
+
+    @Override
+    public void onReceiveLocation(BDLocation location) {
+        //mapView 销毁后不在处理新接收的位置
+        LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+        if (isFirstLocate) {
+
+            isFirstLocate = false;
+            //给地图设置状态
+            mBaiduMap.animateMapStatus( MapStatusUpdateFactory.newLatLng(ll));
+        }
+        if (location == null || mMapView == null){
+            return;
+        }
+        MyLocationData locData = new MyLocationData.Builder()
+                .accuracy(location.getRadius())
+                // 此处设置开发者获取到的方向信息，顺时针0-360
+                .direction(location.getDirection()).latitude(location.getLatitude())
+                .longitude(location.getLongitude()).build();
+        mBaiduMap.setMyLocationData(locData);
+    }
+
+}
